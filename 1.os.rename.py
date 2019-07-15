8215f1c42d2142f24f0c66fbd48a4bac4aaca55e
jitendra singh <33808010+Bahubuli@users.noreply.github.com>
2019-07-15 16:29:41 +0530

    Update and rename rename.py to 1.os.rename.py

--- 1.os.rename.py
+++ 1.os.rename.py
@@ -3,3 +3,6 @@ filelist=os.listdir("D:\downloads\prank\prank")
 print(filelist)
 for  file in filelist:
     os.rename(file,file.strip("0987654321"))
+    
+#os.chdir()= will change current working directory
+#os.getcwd()=will give current working directory
