7e69be2fc7e7d7e17f4d8dc5baf16330083312dd
jitendra singh <jitendra.singh.sola@gmail.com>
2021-07-25 19:30:30 +0530

    ..

---
+++ archieved/emumerate and zip.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+l=["mto", "nhi","hu","insano","me"]
+
+j=["bikta" ,"hu","mto","in","dukamo", "me"]
+
+for i in enumerate(l):
+    print(i)
+    
+
+
+print(list(zip(l,j)))
+
