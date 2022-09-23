ad9945af2121b5844f853cb401a11b740238e7ef
Bahubuli <jitendra.singh.sola@gmail.com>
2022-09-23 20:04:54 +0530

    ..

--- Mini Projects/27. Calculator/script.js
+++ Mini Projects/27. Calculator/script.js
@@ -31,17 +31,17 @@ function operate(num1, operator, num2)
     {
          return mul(num1, num2);
     }
-    if (operator === '+-/')
+    if (operator === '/')
     {
          return divd(num1, num2);
     }
 }
 
-let num1=5, num2=10, operator="+", gotleft = false;
+let num1=0, num2=0, operator="+", got_left = false;
 
 const display = document.querySelector(".display");
 
-display.textContent = operate(num1, operator, num2);
+
 
 const clear = document.querySelector(".clear");
 
@@ -60,9 +60,37 @@ const nums = document.querySelectorAll(".num");
 nums.forEach(num => {
 
     num.addEventListener("click", ()=>{
-        if(gotleft) num2 = num2 + "" + num.textContent;
+        if(got_left===true) num2 = num2 + "" + num.textContent;
         else num1 = num1 + "" + num.textContent;
-    }
 
-   console.log( num1 );
+        console.log(num1);
+        console.log(num2);
+    })
+
+
  })
+
+const add_btn = document.querySelector(".add");
+const sub_btn = document.querySelector(".sub");
+const mul_btn = document.querySelector(".mul");
+const divd_btn = document.querySelector(".divd");
+
+const operations = [add_btn, sub_btn, mul_btn, divd_btn];
+
+operations.forEach(op => {
+
+    op.addEventListener("click", () => {
+        operator = op.textContent;
+        got_left = true;
+     })
+})
+
+const domath = document.querySelector(".equal");
+
+domath.addEventListener("click",()=> {
+
+    got_left = false;
+    display.textContent = operate(parseInt(num1), operator, parseInt(num2))
+}
+
+)
