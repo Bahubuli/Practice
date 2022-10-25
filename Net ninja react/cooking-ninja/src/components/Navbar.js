6b3821ab71b3977a70742e0a795fa7b87e336ba8
Jitendra <jitendra.singh.sola@gmail.com>
2022-10-25 17:10:37 +0530

    context and reducer in react

--- Net ninja react/cooking-ninja/src/components/Navbar.js
+++ Net ninja react/cooking-ninja/src/components/Navbar.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Switch, Route,Link } from 'react-router-dom';
+
 import SearchBar from '../Pages/searchBar/SearchBar';
 import './Navbar.css'
-
+import {useTheme} from '../Hooks/useTheme'
 export default function Navbar() {
-  return (
-      <div className = "navbar">
 
-          <nav>
+    const {color, changeColor } = useTheme()
+
+  return (
+      <div className = "navbar" style = {{background: color}}>
+
+          <nav onClick ={()=>changeColor('red')}>
               <Link to="/" className = "brand">
                     <h1>Cooking Ninja</h1>
               </Link>
